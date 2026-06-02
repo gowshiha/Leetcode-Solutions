cba71046a4ccca51fb9d7390e6db07db22af14b2
gowshiha <gowshiha.v@gmail.com>
2026-06-02 19:34:53 +0530

    Time: 9 ms (6.09%), Space: 46.2 MB (6.83%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        String original = Integer.toString(x);
+        String reversed = new StringBuilder(original).reverse().toString();
+        return original.equals(reversed);
+    }
+}
