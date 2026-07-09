ad80497b446d98bdce2bbea92dd099730dd89844
GOWSHIHA <gowshiha.v@gmail.com>
2026-07-09 09:38:09 +0530

    Time: 4 ms (48.47%), Space: 45.2 MB (39.19%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,30 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s == null) {
-            return false;
+        StringBuilder cleaned = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleaned.append(Character.toLowerCase(c));
+            }
         }
         
-        int left = 0;
-        int right = s.length() - 1;
+        String original = cleaned.toString();
+        String reversed = cleaned.reverse().toString();
         
-        while (left < right) {
-
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
-            }
-
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            }
-
-            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
-                return false;
-            }
-            
-            left++;
-            right--;
-        }
-        
-        return true;
+        return original.equals(reversed);
     }
 }
